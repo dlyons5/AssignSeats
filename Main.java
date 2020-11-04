1b09b8b94253f35181a748e5ffbc701d6f8b528f
dlyons5 <dominic.lyons@patelhs.org>
2020-11-04 17:36:55 +0000

    for loop 1

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ class Main {
    
    int[ ] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
-   for (int i = i++)
+   for (int i = 0; i<= 15; i++){
+      System.out.println(seatNumbers[i]);
    {
       
   }
