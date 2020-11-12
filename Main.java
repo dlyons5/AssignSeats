629bda345b29b6bfa3e1b90ee987b657f2c82f73
dlyons5 <dominic.lyons@patelhs.org>
2020-11-12 20:25:53 +0000

    added some comments

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
  import java.util.Scanner;
-
+//this "import" is for the scanner 
 class Main {
  
   public static void main(String[] args) {
    
    int[] seatNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-
+//15 students and 15 seats 
    for (int i = 0; i<= 15; i++){
       System.out.println(seatNumbers[i]);
    {
